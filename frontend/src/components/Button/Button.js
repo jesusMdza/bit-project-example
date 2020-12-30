358675adb7a0e16dae3d6a8406dcf2082f2e74db
Jesus Mendoza <jesuszmendoza14@gmail.com>
2020-12-30 15:10:28 -0600

    feat(frontend): add correct node-sass, modify component

--- frontend/src/components/Button/Button.js
+++ frontend/src/components/Button/Button.js
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "./Button.module.scss";
 
 const Button = () => {
-  return <div onClick={() => alert()}>Click Me</div>;
+  return (
+    <div className={styles.button} onClick={() => alert("Button Clicked")}>
+      Click Me!
+    </div>
+  );
 };
 
 export default Button;
